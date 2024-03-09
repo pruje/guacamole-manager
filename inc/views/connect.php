a0b9aa23f6aba05faaa96a1c1ebc3a8c6c6d32c9
Jean Prunneaux <jean@prunneaux.com>
2024-03-09 18:03:38 +0100

    Add connect page

---
+++ inc/views/connect.php
@@ -0,0 +1,15 @@
+<?php
+if (!isset($_GET['c']) || $_GET['c'] == '') {
+    http_response_code(404);
+    echo '404 not found';
+    die();
+}
+
+$connection = Connection::getByName(htmlspecialchars($_GET['c']));
+if (!$connection) {
+    http_response_code(404);
+    echo 'Connection not found';
+    die();
+}
+
+header("Location: ../guacamole/#/client/".base64_encode($connection->id."\0c\0".DATABASE));
