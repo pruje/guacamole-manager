03cdb4963952d553a95375970b416bb4fe491fcc
Jean Prunneaux <jean.prunneaux@univ-lorraine.fr>
2024-06-27 14:05:31 +0200

    Get connection groups recursively

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A simple web interface to manage many [Apache Guacamole](https://guacamole.apache.org) connections.
 
+# Installation
+Copy `config.default.php` to `config.php` and make changes.
+
 # Credits
 Author: Jean Prunneaux https://jean.prunneaux.com
 
--- inc/classes/ConnectionGroup.php
+++ inc/classes/ConnectionGroup.php
@@ -2,8 +2,9 @@
 
 class ConnectionGroup
 {
-    protected $id;
-    protected $name;
+    public $id;
+    public $name;
+    public $parent_id;
 
     public function __construct($id = null)
     {
@@ -32,13 +33,52 @@ class ConnectionGroup
     }
 
 
+    public static function getById($id)
+    {
+        $data = (new Database)->selectFirst("SELECT connection_group_name AS name, parent_id
+                                        FROM guacamole_connection_group
+                                        WHERE connection_group_id=?", [$id]);
+        if (!$data) {
+            return false;
+        }
+
+        $group = new self($id);
+        $group->name = $data['name'];
+        $group->parent_id = $data['parent_id'];
+
+        return $group;
+    }
+
+
     /**
      * Get all connection groups
      */
     public static function getAll()
     {
-        return (new Database)->select("SELECT connection_group_id AS id, connection_group_name AS name
+        $groups = (new Database)->select("SELECT connection_group_id AS id, connection_group_name AS name, parent_id
                                        FROM guacamole_connection_group
                                        WHERE connection_group_name <> 'Templates' ORDER BY name");
+
+        foreach ($groups as &$group) {
+            $parent_id = $group['parent_id'];
+            while ($parent_id) {
+                $parent = self::getById($parent_id);
+                if (!$parent) {
+                    break;
+                }
+                $group['name'] = $parent->name . ' > ' . $group['name'];
+                $parent_id = $parent->parent_id;
+            }
+        }
+
+        // you can use array_column() instead of the above code
+        $ids  = array_column($groups, 'id');
+        $names = array_column($groups, 'name');
+
+        // Sort the data with volume descending, edition ascending
+        // Add $data as the last parameter, to sort by the common key
+        array_multisort($names, $ids, $groups);
+
+        return $groups;
     }
 }
